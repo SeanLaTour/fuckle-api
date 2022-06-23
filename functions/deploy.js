b9ae9df2c6cfc6c5d7bbff36ac44a75cb7bb2203
Sean La Tourette <sean.latourette@straightfire.tech>
2022-06-22 19:19:00 -0700

    stuffff

--- functions/deploy.js
+++ functions/deploy.js
@@ -1,71 +1,18 @@
-const express = require('express')
-var cors = require('cors');
-var cron = require('node-cron');
+const { schedule } = require('@netlify/functions')
 
-const CUSS_WORDS = [
-  "FUCK",
-  "SHIT",
-  "PISS",
-  "CUNT",
-  "DICK",
-  "JIZZ",
-  "TITS",
-  "COCK",
-  "DONG",
-  "TWAT",
-  "FART",
-  "POOP",
-  "NIPS",
-  "CRAP",
-  "HELL",
-  "THOT",
-  "DAMN",
-  "SIMP",
-  "ARSE",
-  "WANK",
-  "CUCK",
-  "SLUT",
-  "WANK",
-  "NUTS",
-  "BUTT",
-  "SHAG",
-  "GASH",
-  "JUGS",
-  "MUFF",
-  "BANG",
-  "DIKE",
-  "SLIT",
-  "DUMP",
-  "CLIT",
-  "HUMP",
-  "TURD",
-  "ANAL",
-  "FROT",
-  "GIMP",
-  "PIMP",
-];
 
-const app = express()
-app.use(cors({
-  origin: '*'
-}));
- 
 
-let index = 0
-cron.schedule('* * * * *', () => {
-  console.log('running a task every day');
-  index += 1
-  if (index === CUSS_WORDS.length) index = 0
-  console.log(CUSS_WORDS[index])
-});
+const handlerFn = async function() {
+    let index = 0
+    index += 1
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Authorization, Content-Type"
+        },
+        body: `Number index: ${index}`
+    };
+}; 
 
-var port = process.env.PORT || 3000;
-
-app.get('/', (_, res) => {
-  const word = CUSS_WORDS[index]
-  res.status(200).send(JSON.stringify(word))
-})
-
-app.listen(port, () => console.log(`Running on port ${port}`))
-
-module.exports = app;
+module.exports.handler = schedule('* * * * *', handlerFn)
