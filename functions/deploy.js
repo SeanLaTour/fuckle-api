bda8e6a7b71e8f9127db69d6734ee0e6c1bdba02
Sean La Tourette <sean.latourette@straightfire.tech>
2022-06-22 18:53:51 -0700

    ughdf

--- functions/deploy.js
+++ functions/deploy.js
@@ -4,7 +4,7 @@ const { schedule } = require('@netlify/functions')
 
 const handlerFn = async function(index) {
     let index = 0
-    index += 1
+
     return {
         statusCode: 200,
         body: `Number index: ${index}`
